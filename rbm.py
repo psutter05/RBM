37b9a3cdce45f7d9b0615c5a762e42a36f706bec
Maciek <maciekbis@gmail.com>
2013-11-05 17:38:53 +0000

    Updated the RBM and added different smiley faces, the code is dirty, but it works

--- rbm.py
+++ rbm.py
@@ -1,19 +1,38 @@
 import numpy as np
 import math
-import random
 
 class RBM:
-  def __init__(self, visible, hidden, learning_rate = 0.1):
+  def __init__(self, visible, hidden, learning_rate = 0.01):
     self.visible = visible
     self.hidden = hidden
     self.learning_rate = learning_rate
 
     #initialize the weight with an additional row and column for the bias weights 
-    self.weights = np.random.random((visible+1, hidden+1))
+    self.weights = np.random.normal(0,0.01,(visible+1, hidden+1))
 
+    #set the hidden bias weights to 0
+    self.weights[:,0] = 0
+
+    #set the visible bias weights to the approximate probability of a neuron being activated in the training data
+    self.weights[0,:] = 0.8
+
+  def train(self, data, epochs, classes):
+    data = np.array(data)
+    if classes < 1:
+      self.train2(data, epochs)
+      return
+
+    num_examples = data.shape[0]
+    batch_size = num_examples / classes
+
+    for x in range(0, batch_size):
+      batch = []
+      for i in range(0,classes):
+        batch.append(data[batch_size * i + x])
+      self.train2(batch,epochs)
 
   #Trains the machine on the samples for the given number of epochs
-  def train(self, data, epochs = 9999):
+  def train2(self, data, epochs = 9999):
     data = np.array(data)
     num_examples = data.shape[0]
     for epoch in range(0,epochs):
@@ -40,15 +59,16 @@ class RBM:
       #Calculate the state of the hidden neurons given the visible neurons
       h_act = np.dot(data, self.weights)
       h_prob = self.__logistic(h_act)
-      h_state = (h_prob > random.random()).astype(int)
+      h_state = (h_prob > np.random.random(h_prob.shape)).astype(int)
+      h_state[:,0] = 1
 
       #Regenerate the visible neurons from the hidden ones
       v_act = np.dot(h_state, self.weights.T)
       v_prob = self.__logistic(v_act)
-      v_state = (v_prob > random.random()).astype(int)
+      v_state = (v_prob > np.random.random(v_prob.shape)).astype(int)
 
-      #Set the bias neurons
-      v_state[:,1] = 1
+     # #Set the bias neurons
+     # v_state[:,0] = 1
 
     #Delete the row containing bias neurons
     v_state = np.delete(v_state, 0, axis = 1)
--- smiley_faces/smiley.py
+++ smiley_faces/smiley.py
@@ -10,24 +10,26 @@ import os
 class SFG:
   
   def __init__(self):
-    self.image_width = self.image_height = 28
+    self.image_width = self.image_height = 25
     self.visible_units = self.image_width * self.image_height
-    self.hidden_units = self.visible_units / 10
-    self.rbm = RBM(self.visible_units, self.hidden_units)
+    self.hidden_units = 400
+    self.rbm = RBM(self.visible_units, self.hidden_units, 0.05)
 
   #assumes there are only training images in the training_folder
-  def train(self, training_folder, epochs = 500):
+  def train(self, training_folder, epochs = 200):
     data = []
     for training_image in os.listdir(training_folder):
       image = pil.open(training_folder + '/' + training_image)
       image = self.array_for_image(image)
       data.append(image)
 
-    self.rbm.train(data, epochs)
+    self.rbm.train(data, epochs, 4)
   
   #takes a pil Image and returns an arary of 1s and 0s
   def array_for_image(self, image):
-    return np.array(image.convert("L")).flatten() / 255
+    arr = np.array(image.convert("L")).flatten() > (255 / 2)
+    return arr.astype(np.uint8)
+
 
   def regen_image(self, image, samples):
     data = self.array_for_image(image)
