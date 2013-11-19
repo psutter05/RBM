7bb6b361764aace5a43b988b6ccc1ee33b9c1132
Maciek <maciekbis@gmail.com>
2013-11-19 12:28:21 +0000

    added different sized learning examples

--- big-example.py
+++ big-example.py
@@ -2,27 +2,24 @@ from data import MNIST
 from dbn import DBN
 import numpy as np
 
-d = DBN([28*28, 500,500, 2000], 10,0.1)
+d = DBN([28*28, 500,500, 1000], 10,0.1)
 images = MNIST.load_images('images')
 labels = MNIST.load_labels('labels')
 
-img = images[0:60000]
-lbl = labels[0:60000]
+img = images[0:56000]
+lbl = labels[0:56000]
 
-tst_img = MNIST.load_images('test_images')
-tst_lbl = MNIST.load_labels('test_labels')
+tst_img = images[56000:60000]
+tst_lbl = labels[56000:60000]
 
-d.pre_train(img,5,50)
-for i in xrange(0, 100):
-  d.train_labels(img, lbl, 50, 50)
-  tst_class = d.classify(tst_img,10)
-  print 'Error over test data: {0}'.format(1 - (tst_class*tst_lbl).mean() * 10)
+d.pre_train(img,30)
+d.train_labels(img, lbl,200)
 
 #print d.sample(img[0:1],0) 
 #print d.sample(img[1:2],0) 
 #print 'layer 2'
 #print d.sample(img[0:1],1) 
-#print d.sample(img[1:2],2) 
+#print d.sample(img[1:2],1) 
 #print 'layer 2'
 #print d.sample(img[0:1],2) 
 #print d.sample(img[1:2],2)
@@ -34,7 +31,7 @@ print np.around(d.classify(img, 20), 2)[0:20]
 print lbl[0:20]
 
 tst_class = d.classify(tst_img,20)
-err_test = 1 - ((tst_class * tst_lbl).mean() * 10)
+err_test = 1 - ((tst_class * tst_lbl).mean()) * 10
 
 print "TEST"
 print np.around(tst_class)[0:10]
--- small_example.py
+++ small_example.py
@@ -2,27 +2,27 @@ from data import MNIST
 from dbn import DBN
 import numpy as np
 
-d = DBN([28*28, 500,500, 2000], 10,0.1)
-images = MNIST.load_images('images')
-labels = MNIST.load_labels('labels')
+d = DBN([28*28, 500, 2000], 10,0.1)
+images = MNIST.load_images('test_images')
+labels = MNIST.load_labels('test_labels')
 
-img = images[0:60000]
-lbl = labels[0:60000]
+img = images[0:100]
+lbl = labels[0:100]
 
-tst_img = MNIST.load_images('test_images')
-tst_lbl = MNIST.load_labels('test_labels')
+tst_img = images[9000:95000]
+tst_lbl = labels[9000:95000]
 
-d.pre_train(img,5,50)
+d.pre_train(img,30,50)
 for i in xrange(0, 100):
-  d.train_labels(img, lbl, 50, 50)
-  tst_class = d.classify(tst_img,10)
-  print 'Error over test data: {0}'.format(1 - (tst_class*tst_lbl).mean() * 10)
+  d.train_labels(img, lbl, 3, 50)
+  tst_class = d.classify(tst_img,20)
+  print 'Error kurwa: {0}'.format( 1 - ((tst_class * tst_lbl).mean()) * 10)
 
 #print d.sample(img[0:1],0) 
 #print d.sample(img[1:2],0) 
 #print 'layer 2'
 #print d.sample(img[0:1],1) 
-#print d.sample(img[1:2],2) 
+#print d.sample(img[1:2],1) 
 #print 'layer 2'
 #print d.sample(img[0:1],2) 
 #print d.sample(img[1:2],2)
@@ -34,7 +34,7 @@ print np.around(d.classify(img, 20), 2)[0:20]
 print lbl[0:20]
 
 tst_class = d.classify(tst_img,20)
-err_test = 1 - ((tst_class * tst_lbl).mean() * 10)
+err_test = 1 - ((tst_class * tst_lbl).mean()) * 10
 
 print "TEST"
 print np.around(tst_class)[0:10]
